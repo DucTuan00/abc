e34f52fac180d2afcd28866a21057e170c4e0660
Đức Tuấn <139686944+DucTuan00@users.noreply.github.com>
2023-12-26 09:04:43 +0700

    cmt

---
+++ QuanLyBanHang/frmMain.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace QuanLyBanHang
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuDangXuat = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuDanhMuc = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHangHoa = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHoaDon = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHoaDonBan = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mnuChatLieu = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNhanSu = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNhanVien = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuKhach = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuKhachHang = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuDanhMuc,
+            this.mnuNhanSu,
+            this.mnuKhach,
+            this.mnuHoaDon});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1000, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuDangXuat});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(69, 24);
+            this.mnuFile.Text = "&Tệp tin";
+            // 
+            // mnuDangXuat
+            // 
+            this.mnuDangXuat.Name = "mnuDangXuat";
+            this.mnuDangXuat.Size = new System.Drawing.Size(224, 26);
+            this.mnuDangXuat.Text = "Đăng xuất";
+            this.mnuDangXuat.Click += new System.EventHandler(this.mnuDangXuat_Click);
+            // 
+            // mnuDanhMuc
+            // 
+            this.mnuDanhMuc.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuChatLieu,
+            this.mnuHangHoa});
+            this.mnuDanhMuc.Name = "mnuDanhMuc";
+            this.mnuDanhMuc.Size = new System.Drawing.Size(49, 24);
+            this.mnuDanhMuc.Text = "&Kho";
+            // 
+            // mnuHangHoa
+            // 
+            this.mnuHangHoa.Name = "mnuHangHoa";
+            this.mnuHangHoa.Size = new System.Drawing.Size(224, 26);
+            this.mnuHangHoa.Text = "&Hàng hóa";
+            this.mnuHangHoa.Click += new System.EventHandler(this.mnuHangHoa_Click);
+            // 
+            // mnuHoaDon
+            // 
+            this.mnuHoaDon.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuHoaDonBan});
+            this.mnuHoaDon.Name = "mnuHoaDon";
+            this.mnuHoaDon.Size = new System.Drawing.Size(81, 24);
+            this.mnuHoaDon.Text = "&Hóa đơn";
+            // 
+            // mnuHoaDonBan
+            // 
+            this.mnuHoaDonBan.Name = "mnuHoaDonBan";
+            this.mnuHoaDonBan.Size = new System.Drawing.Size(224, 26);
+            this.mnuHoaDonBan.Text = "&Hóa đơn bán";
+            this.mnuHoaDonBan.Click += new System.EventHandler(this.mnuHoaDonBan_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QuanLyBanHang.Properties.Resources.Screenshot_2023_10_05_214035;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 494);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mnuChatLieu
+            // 
+            this.mnuChatLieu.Name = "mnuChatLieu";
+            this.mnuChatLieu.Size = new System.Drawing.Size(224, 26);
+            this.mnuChatLieu.Text = "&Chất liệu";
+            this.mnuChatLieu.Click += new System.EventHandler(this.mnuChatLieu_Click);
+            // 
+            // mnuNhanSu
+            // 
+            this.mnuNhanSu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuNhanVien});
+            this.mnuNhanSu.Name = "mnuNhanSu";
+            this.mnuNhanSu.Size = new System.Drawing.Size(77, 24);
+            this.mnuNhanSu.Text = "&Nhân sự";
+            // 
+            // mnuNhanVien
+            // 
+            this.mnuNhanVien.Name = "mnuNhanVien";
+            this.mnuNhanVien.Size = new System.Drawing.Size(224, 26);
+            this.mnuNhanVien.Text = "&Nhân viên";
+            this.mnuNhanVien.Click += new System.EventHandler(this.mnuNhanVien_Click);
+            // 
+            // mnuKhach
+            // 
+            this.mnuKhach.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuKhachHang});
+            this.mnuKhach.Name = "mnuKhach";
+            this.mnuKhach.Size = new System.Drawing.Size(63, 24);
+            this.mnuKhach.Text = "&Khách";
+            // 
+            // mnuKhachHang
+            // 
+            this.mnuKhachHang.Name = "mnuKhachHang";
+            this.mnuKhachHang.Size = new System.Drawing.Size(224, 26);
+            this.mnuKhachHang.Text = "&Khách hàng";
+            this.mnuKhachHang.Click += new System.EventHandler(this.mnuKhachHang_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 526);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Ứng dụng quản lý bán quần áo cho Nam";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuDanhMuc;
+        private System.Windows.Forms.ToolStripMenuItem mnuHangHoa;
+        private System.Windows.Forms.ToolStripMenuItem mnuHoaDon;
+        private System.Windows.Forms.ToolStripMenuItem mnuHoaDonBan;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem mnuDangXuat;
+        private System.Windows.Forms.ToolStripMenuItem mnuChatLieu;
+        private System.Windows.Forms.ToolStripMenuItem mnuNhanSu;
+        private System.Windows.Forms.ToolStripMenuItem mnuNhanVien;
+        private System.Windows.Forms.ToolStripMenuItem mnuKhach;
+        private System.Windows.Forms.ToolStripMenuItem mnuKhachHang;
+    }
+}
+
---
+++ QuanLyBanHang/frmMain.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyBanHang.Class;
+
+namespace QuanLyBanHang
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Phần phân quyền
+        void PhanQuyen()
+        {
+            if(Const.TaiKhoan.LoaiTaiKhoan == false)
+            {
+                mnuChatLieu.Enabled = false;
+                mnuHangHoa.Enabled = false;
+                mnuNhanVien.Enabled = false;
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Functions.Connect();
+            PhanQuyen();
+        }
+
+        private void mnuThoat_Click(object sender, EventArgs e)
+        {
+            Functions.Disconnect();
+            Application.Exit();
+        }
+
+        private void mnuChatLieu_Click(object sender, EventArgs e)
+        {
+            frmDMChatLieu frm = new frmDMChatLieu();
+            frm.ShowDialog();
+        }
+
+        private void mnuHangHoa_Click(object sender, EventArgs e)
+        {
+            frmDMHangHoa frm = new frmDMHangHoa();
+           // frm.MdiParent = this;
+            frm.Show();
+        }
+
+        
+
+        private void mnuNhanVien_Click(object sender, EventArgs e)
+        {
+            frmDMNhanVien frm = new frmDMNhanVien();
+            frm.Show();
+        }
+
+        private void mnuKhachHang_Click(object sender, EventArgs e)
+        {
+            frmDMKhachHang frm = new frmDMKhachHang();
+            frm.Show();
+        }
+
+        
+
+        private void mnuHoaDonBan_Click(object sender, EventArgs e)
+        {
+            frmHoaDonBan frm = new frmHoaDonBan();
+            frm.Show();
+        }
+
+        
+
+        //Phần đăng xuất
+        public bool isThoat = true;
+        public event EventHandler DangXuat;
+        private void mnuDangXuat_Click(object sender, EventArgs e)
+        {
+            DangXuat(this, new EventArgs());
+            /*isThoat = false;
+            this.Close();
+            frmDangNhap frm = new frmDangNhap();
+            frm.Show();*/
+        }
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if(isThoat)
+            {
+                Functions.Disconnect();
+                Application.Exit();
+            }
+        }
+
+        
+    }
+}
